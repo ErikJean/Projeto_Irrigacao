1308896a88747d1aa757aa418b99970af0f49250
ErikJean <164931138+ErikJean@users.noreply.github.com>
2024-07-18 16:32:42 -0300

    Update comunicação_arduino.py

--- comunicação_arduino.py
+++ comunicação_arduino.py
@@ -8,19 +8,21 @@ mensagem_recebida = ''
 mensagem_enviada = ''
 
 volume = -1
+irrigando = 0
 
 while True: #Loop para a conexão com o Arduino
     try:  #Tenta se conectar, se conseguir, o loop se encerra
-        arduino = serial.Serial('COM6', 9600)
+        arduino = serial.Serial('COM4', 9600)
         print('Arduino conectado')
         break
     except:
         pass
     
 volume = float(input("Qual o volume a ser irrigado? "))
+irrigando = int(input("Quer iniciar a irrigação (0 ou 1)? "))
 
 if(volume != -1):
-    mensagem_enviada = str(volume) + "*" 
+    mensagem_enviada = str(irrigando) + "-" + str(volume) + "*"
     arduino.write(mensagem_enviada.encode())
     arduino.flush()
 
