ff89b7bf5169352cbea896dc5bb0b8831aff1b9d
ErikJean <164931138+ErikJean@users.noreply.github.com>
2024-07-18 14:01:48 -0300

    Update Comunication_Python.ino

--- Comunication_Python.ino
+++ Comunication_Python.ino
@@ -1,8 +1,16 @@
-unsigned long tempo_gravado = 0; // Para armazenar um tempo refente a execução do arduino
-float volume = 0;
-float volume_irrigado;
+// Para armazenar um tempo refente a execução do arduino
+unsigned long tempo_gravado = 0;
+
+//Variáveis para controle de dados
 String mensagem; // Controle de dados
 char caractere; // Controle de dados
+bool irrigando = 0; // Irrigação desativada ou ativada
+
+
+//Definicao das variáveis de fluxo e volume
+float volume = 0;
+float volume_irrigado;
+float fluxo = 0;
 
 
 void setup() {
@@ -10,12 +18,19 @@ void setup() {
 }
 
 void loop() {
+  noInterrupts();
+
   if(Serial.available()) {
     caractere = Serial.read();
 
     if(caractere == '*') {
       volume = mensagem.toFloat();
       mensagem = "";
+    }
+
+    else if(caractere == '-') {
+      irrigando = mensagem.toInt();
+      mensasagem = "";
     }
 
     else {
@@ -26,6 +41,8 @@ void loop() {
   if((millis() - tempo_gravado) > 1000) {
     Serial.flush();
     Serial.print("<");
+    Serial.print(irrigando);
+    Serial.print(";");
     Serial.print(volume);
     Serial.print(";");
     Serial.print(volume_irrigado);
