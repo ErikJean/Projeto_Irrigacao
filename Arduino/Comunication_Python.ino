052206ca726b0c1edbb2ebfa1414bd1d004abbba
ErikJean <164931138+ErikJean@users.noreply.github.com>
2024-07-21 13:03:31 -0300

    Update Comunication_Python.ino

--- Arduino/Comunication_Python.ino
+++ Arduino/Comunication_Python.ino
@@ -1,19 +1,19 @@
 // Para armazenar um tempo refente a execução do arduino
-unsigned long tempo_gravado = 0;
+unsigned long tempoGravado = 0;
 
 // Variáveis para controle de dados
-String mensagem; // Controle de dados
-char caractere; // Controle de dados
-bool irrigando = 0; // Irrigação desativada ou ativada
+String mensagem;     // Controle de dados
+char caractere;      // Controle de dados
+bool irrigando = 0;  // Irrigação desativada ou ativada
 
 
 // Definicao das variáveis de fluxo e volume
 float volume = 0;
-float volume_irrigado;
+float volumeIrrigado;
 float fluxo = 0;
 
 // Definicao do fator de calibracao para conversao do valor lido
-float FATOR_CALIBRACAO = 4.7;
+float fatorCalibracao = 4.7;
 
 
 void setup() {
@@ -21,20 +21,20 @@ void setup() {
 }
 
 void loop() {
-  if(Serial.available()) { // Leitura da comunicação
+  if (Serial.available()) {  // Leitura da comunicação
     caractere = Serial.read();
 
-    if(caractere == '*') {
+    if (caractere == '*') {
       volume = mensagem.toFloat();
       mensagem = "";
     }
 
     else if (caractere == ';') {
-      FATOR_CALIBRACAO = mensagem.toFloat();
+      fatorCalibracao = mensagem.toFloat();
       mensagem = "";
     }
 
-    else if(caractere == '-') {
+    else if (caractere == '-') {
       irrigando = mensagem.toInt();
       mensagem = "";
     }
@@ -44,17 +44,46 @@ void loop() {
     }
   }
 
-  if((millis() - tempo_gravado) > 1000) { // Impressão no monitor serial
-    Serial.print("<");
-    Serial.print(irrigando);
-    Serial.print(";");
-    Serial.print(FATOR_CALIBRACAO);
-    Serial.print(";");
-    Serial.print(volume);
-    Serial.print(";");
-    Serial.print(volume_irrigado);
-    Serial.println(">");
+  print();
 
-    tempo_gravado = millis();
-  } 
+  while (irrigando == 1) {
+    if (Serial.available()) {  // Leitura da comunicação
+      caractere = Serial.read();
+
+      if (caractere == '*') {
+        mensagem = "";
+      }
+
+      else if (caractere == ';') {
+        mensagem = "";
+      }
+
+      else if (caractere == '-') {
+        irrigando = mensagem.toInt();
+        mensagem = "";
+      }
+
+      else {
+        mensagem.concat(caractere);
+      }
+    }
+
+    print();
+  }
+}
+
+void print() {
+  if ((millis() - tempoGravado) > 1000) {  // Impressão no monitor serial
+      Serial.print("<");
+      Serial.print(irrigando);
+      Serial.print(";");
+      Serial.print(fatorCalibracao);
+      Serial.print(" | ");
+      Serial.print(volume);
+      Serial.print(";");
+      Serial.print(volumeIrrigado);
+      Serial.println(">");
+
+      tempoGravado = millis();
+    }
 }
