b76f46a23820850b183f882db42b8680bf06b1d6
ErikJean <164931138+ErikJean@users.noreply.github.com>
2024-07-21 13:40:38 -0300

    Update 1.ino

--- Arduino/1.ino
+++ Arduino/1.ino
@@ -1,52 +1,47 @@
-#define RELE_BOMBA 2
-#define RELE_VALVULA 4
+#define PINO_SENSOR 15
 
-//definicao do pino do sensor e de interrupcao
-const int INTERRUPCAO_SENSOR = 15;  //interrupt = 0 equivale ao pino digital 2
-const int PINO_SENSOR = 15;
+// Para armazenar um tempo refente a execução do arduino
+unsigned long tempoGravado = 0;
 
 //definicao da variavel de contagem de voltas
 unsigned long contador = 0;
 
-// Para armazenar um tempo refente a execução do arduino
-unsigned long tempo_gravado = 0;
+// Variáveis para controle de dados
+String mensagem;     // Controle de dados
+char caractere;      // Controle de dados
+bool irrigando = 0;  // Irrigação desativada ou ativada
 
-//definicao do fator de calibracao para conversao do valor lido
-float FATOR_CALIBRACAO = 4.7;
+float volumeAPI = 0;
 
-//Variáveis para controle de dados
-String mensagem; // Controle de dados
-char caractere; // Controle de dados
-bool irrigando = 0; // Irrigação desativada ou ativada
-
-//Definicao das variáveis de fluxo e volume
-float volume = 0;
-float volume_irrigado = 0;
+// Definicao das variáveis de fluxo e volume
 float fluxo = 0;
+float volume = 0;
+float volumeIrrigado = 0;
+
+// Definicao do fator de calibracao para conversao do valor lido
+float fatorCalibracao = 4.7;
 
 
 void setup() {
   Serial.begin(9600);
-  pinMode(RELE_BOMBA, OUTPUT);
-  pinMode(RELE_VALVULA, OUTPUT);
-  digitalWrite(RELE_BOMBA, !HIGH);
-  digitalWrite(RELE_VALVULA, !HIGH);
   pinMode(PINO_SENSOR, INPUT_PULLUP);
 }
 
 void loop() {
-  digitalWrite(RELE_VALVULA, LOW);
-  digitalWrite(RELE_BOMBA, LOW);
-
-  if(Serial.available()) {
+  if (Serial.available()) {  // Leitura da comunicação
     caractere = Serial.read();
 
-    if(caractere == '*') {
-      volume = mensagem.toFloat();
+    if (caractere == '*') {
+      volumeAPI = mensagem.toFloat();
       mensagem = "";
     }
 
-    else if(caractere == '-') {
+    else if (caractere == ';') {
+      fatorCalibracao = mensagem.toFloat();
+      mensagem = "";
+    }
+
+    else if (caractere == '-') {
       irrigando = mensagem.toInt();
       mensagem = "";
     }
@@ -56,55 +51,87 @@ void loop() {
     }
   }
 
-  if(irrigando == 1) {
-    volume_irrigado = 0;
-    tempo_gravado = 0;
+  if ((millis() - tempoGravado) > 1000) {  // Impressão no monitor serial
+    Serial.print("<");
+    Serial.print(irrigando);
+    Serial.print(";");
+    Serial.print(fatorCalibracao);
+    Serial.print(" | ");
+    Serial.print(volumeAPI);
+    Serial.print(";");
+    Serial.print(volumeIrrigado);
+    Serial.println(">");
 
-    while(volume_irrigado < volume && irrigando == 1) {
-      digitalWrite(RELE_VALVULA, HIGH);
-      digitalWrite(RELE_BOMBA, HIGH);
-      sensor_fluxo();
+    tempoGravado = millis();
+  }
+
+  while (irrigando == 1) {
+    sensorFluxo();
+
+    if (Serial.available()) {  // Leitura da comunicação
+      caractere = Serial.read();
+
+      if (caractere == '*') {
+        mensagem = "";
+      }
+
+      else if (caractere == ';') {
+        mensagem = "";
+      }
+
+      else if (caractere == '-') {
+        irrigando = mensagem.toInt();
+        mensagem = "";
+
+        if (irrigando == 0) {
+          detachInterrupt(digitalPinToInterrupt(PINO_SENSOR));
+          tempoGravado = 0;
+        }
+      }
+
+      else {
+        mensagem.concat(caractere);
+      }
     }
-
-    digitalWrite(RELE_VALVULA, LOW);
-    digitalWrite(RELE_BOMBA, LOW);
   }
 }
 
-void sensor_fluxo() {
-  if ((millis() - tempo_gravado) > 1000) {
+void sensorFluxo() {
+  if ((millis() - tempoGravado) > 1000) {
 
-    //desabilita a interrupcão para realizar a conversão do valor de pulsos
-    detachInterrupt(INTERRUPCAO_SENSOR);
+    //desabilita a interrupcao para realizar a conversao do valor de pulsos
+    detachInterrupt(digitalPinToInterrupt(PINO_SENSOR));
 
-    //conversão do valor de pulsos para L/min
-    fluxo = ((1000.0 / (millis() - tempo_gravado)) * contador) / FATOR_CALIBRACAO;
+    //conversao do valor de pulsos para L/min
+    fluxo = ((1000.0 / (millis() - tempoGravado)) * contador) / fatorCalibracao;
 
     //calculo do volume em L passado pelo sensor
     volume = fluxo / 60;
 
     //armazenamento do volume
-    volume_irrigado += volume;
+    volumeIrrigado += volume;
 
     Serial.print("<");
     Serial.print(irrigando);
     Serial.print(";");
-    Serial.print(volume);
+    Serial.print(fatorCalibracao);
+    Serial.print(" | ");
+    Serial.print(volumeAPI);
     Serial.print(";");
-    Serial.print(volume_irrigado);
+    Serial.print(volumeIrrigado);
     Serial.println(">");
 
-    //reinicializacão do contador de pulsos
+    //reinicializacao do contador de pulsos
     contador = 0;
 
-    //atualizacão da variável tempo_antes
-    tempo_gravado = millis();
+    //atualizacao da variavel tempo_antes
+    tempoGravado = millis();
 
     //contagem de pulsos do sensor
-    attachInterrupt(digitalPinToInterrupt(INTERRUPCAO_SENSOR), contador_pulso, FALLING);
+    attachInterrupt(digitalPinToInterrupt(PINO_SENSOR), contadorPulso, FALLING);
   }
 }
 
-void contador_pulso() {
+void contadorPulso() {
   contador++;
 }
