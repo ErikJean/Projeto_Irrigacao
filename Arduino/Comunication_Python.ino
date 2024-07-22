525dc3320d9891b3cc2a7d6ace6c96f467a7d7ae
ErikJean <164931138+ErikJean@users.noreply.github.com>
2024-07-22 17:19:24 -0300

    Update Comunication_Python.ino

--- Arduino/Comunication_Python.ino
+++ Arduino/Comunication_Python.ino
@@ -1,16 +1,22 @@
+#define PINO_SENSOR 15
+
 // Para armazenar um tempo refente a execução do arduino
 unsigned long tempoGravado = 0;
+
+//definicao da variavel de contagem de voltas
+unsigned long contador = 0;
 
 // Variáveis para controle de dados
 String mensagem;     // Controle de dados
 char caractere;      // Controle de dados
 bool irrigando = 0;  // Irrigação desativada ou ativada
 
+float volumeAPI = 0;
 
 // Definicao das variáveis de fluxo e volume
-float volume = 0;
-float volumeIrrigado;
 float fluxo = 0;
+float volume = 0;
+float volumeIrrigado = 0;
 
 // Definicao do fator de calibracao para conversao do valor lido
 float fatorCalibracao = 4.7;
@@ -18,14 +24,16 @@ float fatorCalibracao = 4.7;
 
 void setup() {
   Serial.begin(9600);
+  pinMode(PINO_SENSOR, INPUT_PULLUP);
 }
 
 void loop() {
+  volumeIrrigado = 0;
   if (Serial.available()) {  // Leitura da comunicação
     caractere = Serial.read();
 
     if (caractere == '*') {
-      volume = mensagem.toFloat();
+      volumeAPI = mensagem.toFloat();
       mensagem = "";
     }
 
@@ -44,9 +52,23 @@ void loop() {
     }
   }
 
-  print();
+  if ((millis() - tempoGravado) > 1000) {  // Impressão no monitor serial
+    Serial.print("<");
+    Serial.print(irrigando);
+    Serial.print(";");
+    Serial.print(fatorCalibracao);
+    Serial.print(" | ");
+    Serial.print(volumeAPI);
+    Serial.print(";");
+    Serial.print(volumeIrrigado);
+    Serial.println(">");
+
+    tempoGravado = millis();
+  }
 
   while (irrigando == 1) {
+    sensorFluxo();
+
     if (Serial.available()) {  // Leitura da comunicação
       caractere = Serial.read();
 
@@ -61,29 +83,60 @@ void loop() {
       else if (caractere == '-') {
         irrigando = mensagem.toInt();
         mensagem = "";
+
+        if (irrigando == 0) {
+          detachInterrupt(digitalPinToInterrupt(PINO_SENSOR));
+          tempoGravado = 0;
+        }
       }
 
       else {
         mensagem.concat(caractere);
       }
     }
-
-    print();
   }
 }
 
-void print() {
-  if ((millis() - tempoGravado) > 1000) {  // Impressão no monitor serial
-      Serial.print("<");
-      Serial.print(irrigando);
-      Serial.print(";");
-      Serial.print(fatorCalibracao);
-      Serial.print(" | ");
-      Serial.print(volume);
-      Serial.print(";");
-      Serial.print(volumeIrrigado);
-      Serial.println(">");
+void sensorFluxo() {
+  if ((millis() - tempoGravado) > 1000) {
 
-      tempoGravado = millis();
+    //desabilita a interrupcao para realizar a conversao do valor de pulsos
+    detachInterrupt(digitalPinToInterrupt(PINO_SENSOR));
+
+    //conversao do valor de pulsos para L/min
+    fluxo = ((1000.0 / (millis() - tempoGravado)) * contador) / fatorCalibracao;
+
+    //calculo do volume em L passado pelo sensor
+    volume = fluxo / 60;
+
+    //armazenamento do volume
+    volumeIrrigado += volume;
+
+    if (volumeIrrigado >= volumeAPI) {
+      irrigando = 0;
     }
+
+    Serial.print("<");
+    Serial.print(irrigando);
+    Serial.print(";");
+    Serial.print(fatorCalibracao);
+    Serial.print(" | ");
+    Serial.print(volumeAPI);
+    Serial.print(";");
+    Serial.print(volumeIrrigado);
+    Serial.println(">");
+
+    //reinicializacao do contador de pulsos
+    contador = 0;
+
+    //atualizacao da variavel tempo_antes
+    tempoGravado = millis();
+
+    //contagem de pulsos do sensor
+    attachInterrupt(digitalPinToInterrupt(PINO_SENSOR), contadorPulso, FALLING);
+  }
+}
+
+void contadorPulso() {
+  contador++;
 }
